b320a64b14e5c2ec1b213d06ffd1a0d6f697ca9d
Jo <jtl.sitompul@gmail.com>
2022-02-10 15:59:21 +0800

    Base Project

---
+++ NookSolution/NookApplication/Models/Rentee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NookApplication.Models
+{
+    public class Rentee : User
+    {
+        List<Interest> interests;
+        double Fee { get; set; }
+    }
+}
