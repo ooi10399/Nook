c35a8930f3b5c06875807f7bd68c2dc4790e0ca9
Jo <jtl.sitompul@gmail.com>
2022-02-11 15:34:16 +0800

    New Base Project

---
+++ NookSolution/NookApplication/Migrations/20220211063218_Init.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NookApplication.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interests",
+                columns: table => new
+                {
+                    IntId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IntName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interests", x => x.IntId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rentees",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fee = table.Column<double>(type: "float", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NickName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<bool>(type: "bit", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rentees", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Renters",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NickName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<bool>(type: "bit", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Renters", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InterestRentee",
+                columns: table => new
+                {
+                    InterestsIntId = table.Column<int>(type: "int", nullable: false),
+                    RenteesUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InterestRentee", x => new { x.InterestsIntId, x.RenteesUserId });
+                    table.ForeignKey(
+                        name: "FK_InterestRentee_Interests_InterestsIntId",
+                        column: x => x.InterestsIntId,
+                        principalTable: "Interests",
+                        principalColumn: "IntId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InterestRentee_Rentees_RenteesUserId",
+                        column: x => x.RenteesUserId,
+                        principalTable: "Rentees",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Interests",
+                columns: new[] { "IntId", "IntName" },
+                values: new object[,]
+                {
+                    { 1, "Cafe-hopping" },
+                    { 2, "Pottery-making" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rentees",
+                columns: new[] { "UserId", "About", "DOB", "Email", "Fee", "FullName", "Gender", "NickName", "Password" },
+                values: new object[,]
+                {
+                    { 101, "Call me queen.", new DateTime(1974, 7, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "elicam@email.com", 20.0, "Elizabeth Cambridge", false, "Eli", "elipass" },
+                    { 102, "Call me king.", new DateTime(1974, 7, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "philc@email.com", 20.0, "Phillip Cambridge", true, "Phil", "philpass" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Renters",
+                columns: new[] { "UserId", "About", "DOB", "Email", "FullName", "Gender", "NickName", "Password" },
+                values: new object[,]
+                {
+                    { 101, "Will call you queen.", new DateTime(1974, 7, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "johndoe@email.com", "John Doe", true, "John", "johnpass" },
+                    { 102, "Will call you king.", new DateTime(1974, 7, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "janedoe@email.com", "Jane Doe", false, "Jane", "janepass" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterestRentee_RenteesUserId",
+                table: "InterestRentee",
+                column: "RenteesUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InterestRentee");
+
+            migrationBuilder.DropTable(
+                name: "Renters");
+
+            migrationBuilder.DropTable(
+                name: "Interests");
+
+            migrationBuilder.DropTable(
+                name: "Rentees");
+        }
+    }
+}
---
+++ NookSolution/NookApplication/Models/NookContext.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NookApplication.Models
+{
+    public class NookContext : DbContext
+    {
+        public NookContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Interest> Interests { get; set; }
+        public DbSet<Rentee> Rentees { get; set; }
+        public DbSet<Renter> Renters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // RENTEE
+            modelBuilder.Entity<Rentee>().HasData(
+                new Rentee()
+                {
+                    UserId = 101,
+                    Email = "elicam@email.com",
+                    FullName = "Elizabeth Cambridge",
+                    NickName = "Eli",
+                    DOB = new DateTime(1974, 7, 10),
+                    Gender = false,
+                    Password = "elipass",
+                    About = "Call me queen.",
+                    Fee = 20.0
+                },
+                new Rentee()
+                {
+                    UserId = 102,
+                    Email = "philc@email.com",
+                    FullName = "Phillip Cambridge",
+                    NickName = "Phil",
+                    DOB = new DateTime(1974, 7, 10),
+                    Gender = true,
+                    Password = "philpass",
+                    About = "Call me king.",
+                    Fee = 20.0
+                });
+
+            // RENTER
+            modelBuilder.Entity<Renter>().HasData(
+                new Renter()
+                {
+                    UserId = 101,
+                    Email = "johndoe@email.com",
+                    FullName = "John Doe",
+                    NickName = "John",
+                    DOB = new DateTime(1974, 7, 10),
+                    Gender = true,
+                    Password = "johnpass",
+                    About = "Will call you queen."
+                },
+
+                new Renter()
+                {
+                    UserId = 102,
+                    Email = "janedoe@email.com",
+                    FullName = "Jane Doe",
+                    NickName = "Jane",
+                    DOB = new DateTime(1974, 7, 10),
+                    Gender = false,
+                    Password = "janepass",
+                    About = "Will call you king."
+                });
+
+            // INTEREST
+            modelBuilder.Entity<Interest>().HasData(
+                new Interest()
+                {
+                    IntId = 1,
+                    IntName = "Cafe-hopping"
+                },
+
+                new Interest()
+                {
+                    IntId = 2,
+                    IntName = "Pottery-making"
+                });
+        }
+    }
+}
